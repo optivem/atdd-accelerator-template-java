0dc84fa0a4f556aa5c8ab03440041a7fade573ea
Valentina Jemuović <valentina.jemuovic@optivem.com>
2025-10-15 11:56:59 +0200

    Refactor TodoController to use configurable API base URL from application properties

--- monolith/src/main/java/com/optivem/atddaccelerator/template/monolith/controllers/TodoController.java
+++ monolith/src/main/java/com/optivem/atddaccelerator/template/monolith/controllers/TodoController.java
@@ -1,6 +1,7 @@
 package com.optivem.atddaccelerator.template.monolith.controllers;
 
 import com.optivem.atddaccelerator.template.monolith.models.Todo;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,12 +11,15 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class TodoController {
 
+    @Value("${todos.api.base-url}")
+    private String todosApiBaseUrl;
+
     private final RestTemplate restTemplate = new RestTemplate();
 
     @GetMapping("/todos/{id}")
     @ResponseBody
     public Todo getTodo(@PathVariable int id) {
-        String url = "https://jsonplaceholder.typicode.com/todos/" + id;
+        String url = todosApiBaseUrl + id;
         return restTemplate.getForObject(url, Todo.class);
     }
 }
