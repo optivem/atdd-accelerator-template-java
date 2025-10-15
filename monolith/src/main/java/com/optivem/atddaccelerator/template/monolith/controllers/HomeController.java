608170bb494e5ae3fa8927e120abc0bf6bbab433
Valentina Jemuović <valentina.jemuovic@optivem.com>
2025-10-15 12:03:30 +0200

    Add todos.html for fetching and displaying todo details

--- monolith/src/main/java/com/optivem/atddaccelerator/template/monolith/controllers/HomeController.java
+++ monolith/src/main/java/com/optivem/atddaccelerator/template/monolith/controllers/HomeController.java
@@ -18,4 +18,9 @@ public class HomeController {
     public String echo() {
         return "Echo";
     }
+
+    @GetMapping("/todos")
+    public String todos() {
+        return "todos.html";
+    }
 }
