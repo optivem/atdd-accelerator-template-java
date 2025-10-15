1f57368422631407e6ee1fda47728ff89c1b951b
Valentina Jemuović <valentina.jemuovic@optivem.com>
2025-10-15 12:22:56 +0200

    Restore HomeController to return index.html and add initial HTML structure for the home page

--- monolith/src/main/java/com/optivem/atddaccelerator/template/monolith/controllers/web/HomeController.java
+++ monolith/src/main/java/com/optivem/atddaccelerator/template/monolith/controllers/web/HomeController.java
@@ -2,14 +2,12 @@ package com.optivem.atddaccelerator.template.monolith.controllers.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    @ResponseBody
     public String home() {
-        return "Hello World!";
+        return "index.html";
     }
 }
